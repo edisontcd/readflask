f466e3908c5801c01431fa6593f93361ebd5809a
Edison <edisontcd@yeah.net>
2023-08-20 22:31:23 +0800

    edit __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,29 @@
 from . import json as json
+
+# app.py  执行中央WSGI应用对象
 from .app import Flask as Flask
 from .app import Request as Request
 from .app import Response as Response
+
+# blueprint.py  蓝本处理
 from .blueprint import Blueprint as Blueprint
+
+# config.py  实现与配置相关的对象
 from .config import Config as Config
+
+# ctx.py  实现保存上下文所需的对象
+from .ctx import after_this_request as after_this_request
+from .ctx import copy_current_request_context as copy_current_request_context
+from .ctx import has_app_context as has_app_context
+from .ctx import has_request_context as has_request_context
+
+# globals.py  定义全局对象 >> 代理当前活动
+from .globals import current_app as current_app
+from .globals import g as g
+from .globals import request as request
+from .globals import session as session
+
+# helpers.py  帮助信息
+from .helpers import abort as abort
+from .helpers import flash as flash
+from .helpers import get_flashed_messages as get_flashed_messages
