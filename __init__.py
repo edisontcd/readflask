c6380c438dbfefc8166bae55e40a70e23b9014ff
Edison <edisontcd@yeah.net>
2023-08-20 15:24:28 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from . import json as json
+from .app import Flask as Flask
+from .app import Request as Request
+from .app import Response as Response
+from .blueprint import Blueprint as Blueprint
+from .config import Config as Config
