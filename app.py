1c4da20068f2d638d8a0070b193c3de64c1a2ba9
Edison <edisontcd@yeah.net>
2023-09-21 21:11:17 +0800

    reading app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# 它从 Python 3.7 开始引入，用于提供更好的类型提示功能。
 from __future__ import annotations
 
 # os模块负责程序与操作系统的交互，提供了访问操作系统底层的接口。
@@ -7,7 +8,8 @@ import os
 # 用于操控python的运行时环境。
 import sys
 
-# 对类型提示的支持
+# typing 模块是 Python 3.5 及更高版本引入的一个标准库模块，
+# 用于支持类型提示和类型注解，以提高代码的可读性和可维护性。
 import typing as t
 
 # 弱引用的主要用途是实现保存大对象的高速缓存或映射，
@@ -29,7 +31,8 @@ from inspect import iscoroutinefunction
 # 直到耗尽所有可迭代对象中的元素。可将多个序列处理为单个序列。
 from itertools import chain
 
-# types定义了一些工具函数，用于协助动态创建新的类型。Traceback异常处理.
+# types定义了一些工具函数，用于协助动态创建新的类型。
+# TracebackType 是 Python 的内置类型之一，用于表示异常的回溯信息（traceback）。
 from types import TracebackType
 
 # urllib.parse用于将统一资源定位符（URL）字符串拆分为不同部分（协议、网络位置、路径等），
@@ -40,17 +43,32 @@ from urllib.parse import quote as _url_quote
 import click
 
 # Python的WSGI规范的实用函数库
+# 创建、修改和处理 HTTP 请求或响应的头部信息。
 from werkzeug.datastructures import Headers
+# 表示一个不可变的字典，即一旦创建，就无法更改其内容。
 from werkzeug.datastructures import ImmutableDict
+# 通常用于表示客户端请求中的键错误或缺失。
 from werkzeug.exceptions import BadRequestKeyError
+# 用于表示 HTTP 协议相关的异常情况，例如客户端错误（4xx 错误）
+# 和服务器错误（5xx 错误）等。
 from werkzeug.exceptions import HTTPException
+# 用于表示服务器内部错误的情况。
 from werkzeug.exceptions import InternalServerError
+# 用于处理 URL 构建错误的情况。
 from werkzeug.routing import BuildError
+# 用于执行 URL 到视图函数的映射，以及从视图函数生成 URL。
+# 它允许你动态地构建 URL 和路由请求，以便与 Web 应用程序的路由系统进行交互。
 from werkzeug.routing import MapAdapter
+# 用于表示请求需要重定向到另一个 URL 的情况。
 from werkzeug.routing import RequestRedirect
+# 用于表示与 URL 路由相关的异常情况。
 from werkzeug.routing import RoutingException
+# 定义 URL 路由规则。
 from werkzeug.routing import Rule
+# 监视脚本文件的更改，并在文件更改时自动重新启动应用程序，
+# 以便开发人员可以在不停止服务器的情况下进行代码修改和调试。
 from werkzeug.serving import is_running_from_reloader
+# 构建 HTTP 响应对象。
 from werkzeug.wrappers import Response as BaseResponse
 
 # 命令行应用
@@ -62,6 +80,8 @@ from . import typing as ft
 # 实现保存上下文所需的对象
 from .ctx import AppContext
 from .ctx import RequestContext
+
+
 
 
 
