26e171567435b71560546639ecb944e70deb8f2c
Edison <edisontcd@yeah.net>
2024-09-01 18:24:55 +0800

    __init__.py

---
+++ json/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import json as _json
+import typing as t
+
+from ..globals import current_app
+from .provider import _default
+
+if t.TYPE_CHECKING:  # pragma: no cover
+    from ..wrappers import Response
